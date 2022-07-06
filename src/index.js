4364b3d6ead90b272934e9d670636310f35b2b05
riley soloner <rileysoloner@gmail.com>
2022-07-06 15:23:23 -0400

    mock code challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 // write your code here
+let form = document.querySelector('#new-ramen')
+let newName = document.querySelector('#new-name')
+let newRestaurant = document.querySelector('#new-restaurant')
+let newImage = document.querySelector('#new-image')
+let newComment = document.querySelector('#new-comment')
+let ramenMenu = document.querySelector('#ramen-menu')
+
+let entries = [newName, newRestaurant, newImage, newComment]
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let input = event.target.entries
+    fetch(`http://localhost:3000/${ramens}`)
+    .then(req => req.json())
+    .then(data => console.log(data))
+})
