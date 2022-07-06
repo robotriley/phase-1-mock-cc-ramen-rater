281e80f8c1f502f71abbe1f1f6b439b6be8de78e
riley soloner <rileysoloner@gmail.com>
2022-07-06 14:38:43 -0400

    commit tree c32ebb904e105204784694e1927a9b007e7036f8

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // write your code here
+let form = document.querySelector('#new-ramen')
+let newName = document.querySelector('#new-name')
+let newRestaurant = document.querySelector('#new-restaurant')
+let newImage = document.querySelector('#new-image')
+let newComment = document.querySelector('#new-comment')
+let ramenMenu = document.querySelector('#ramen-menu')
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let input = event.target.searchByID
+    fetch(`http://localhost:3000/${input.id}`)
+    .then(req => req.json())
+    .then(data => console.log(data))
+})
